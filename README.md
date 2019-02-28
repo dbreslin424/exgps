36c066464d145ff5873b2ff8d561b8a6ec119974
Dan <dbreslin424@gmail.com>
2019-02-27 23:50:35 -0500

    added basic single-file support

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-#exgps
+# exgps
 
 Photo GPS coordinate extractor tool that generates KML files
+
+## Example
+
+```
+node exgps.js -f [JPG file path]
+> Latitude: 55,55,55
+> Longitude 55,55,55
+```
+
+## Usage
+
+## Options
--- exgps.js
+++ exgps.js
@@ -1,13 +1,48 @@
 const minimist = require('minimist');
+const ExifImage = require('exif').ExifImage;
 
 //TODO CLI integration
-const argv = minimist(process.argv.slice(2));
 
-console.log(argv);
+const args = minimist(process.argv.slice(2));
+let cmd = args._[0];
+
+if (args.version || args.v) {
+  cmd = 'version';
+}
+
+switch (cmd) {
+  case !cmd:
+    break;
+  case 'version':
+    require('./cmds/version.js')();
+    break;
+  default:
+    //console.error(`"${cmd}" is not a valid command!`);
+    break;
+}
 
 //TODO load files from folder
 
 //TODO extract GPS metadata from EXIF
+//TODO extract to module
+const file = args.f || args.file;
+
+try {
+  new ExifImage({ image: file }, function(error, exifData) {
+    if (error) {
+      console.log('Error: ' + error.message);
+      return;
+    }
+
+    const lat = exifData.gps.GPSLatitude.join(',');
+    const long = exifData.gps.GPSLongitude.join(',');
+
+    console.log('Latitude:', lat);
+    console.log('Longitude', long);
+  });
+} catch (error) {
+  console.log('Error: ' + error.message);
+}
 
 //TODO render KML document
 
